9a48389a75cf00257966a4c2f309729c6b41f4c3
Satyam23092003 <satyam85409@gmail.com>
2025-07-21 16:14:57 +0530

    Refactor webhook handling and restore server configuration

--- server/server.js
+++ server/server.js
@@ -1,36 +1,82 @@
-  import "./config/instrument.js";
-  import express from "express";
-  import cors from "cors";
-  import "dotenv/config";
-  import connectDB from "./config/db.js";
+  // import "./config/instrument.js";
+  // import express from "express";
+  // import cors from "cors";
+  // import "dotenv/config";
+  // import connectDB from "./config/db.js";
   
-  import * as Sentry from "@sentry/node";
-  import {clerkWebhooks} from "./controllers/webhooks.js";
-  import {clerkMiddleware} from '@clerk/express'
+  // import * as Sentry from "@sentry/node";
+  // import {clerkWebhooks} from "./controllers/webhooks.js";
+  // // import {clerkMiddleware} from '@clerk/express'
 
-  const app = express();
-  await connectDB();
-  //middlewares
-  app.use(cors());
-  app.use(express.json());
-  app.use(clerkMiddleware())
+  // const app = express();
+  // await connectDB();
+  // //middlewares
+  // app.use(cors());
+  // app.use(express.json());
+  // // app.use(clerkMiddleware())
 
-  //connect to the database
+  // //connect to the database
 
 
-  //Routes
-  app.get("/", (req, res) => res.send("API working"));
-  app.get("/debug-sentry", function mainHandler(req, res) {
-          throw new Error("My first Sentry error!");
-  });
+  // //Routes
+  // app.get("/", (req, res) => res.send("API working"));
+  // app.get("/debug-sentry", function mainHandler(req, res) {
+  //         throw new Error("My first Sentry error!");
+  // });
 
+  // // app.post("/webhooks", clerkWebhooks);
   // app.post("/webhooks", clerkWebhooks);
-  app.post("/webhooks", clerkWebhooks);
 
-  Sentry.setupExpressErrorHandler(app);
+  // Sentry.setupExpressErrorHandler(app);
   
-  const PORT = process.env.PORT || 5000;
+  // const PORT = process.env.PORT || 5000;
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port:${PORT}`);
-  });
+  // app.listen(PORT, () => {
+  //   console.log(`Server is running on port:${PORT}`);
+  // });
+
+
+  import "./config/instrument.js";
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
+import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from "./controllers/webhooks.js";
+import {clerkMiddleware} from "@clerk/express"
+
+// Initialize the app
+const app = express();
+
+// Connecting to DB
+await connectDB();
+
+// connect to clodinary
+
+ 
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My First Sentry Error");
+});
+
+app.post("/webhooks", clerkWebhooks);
+
+
+// Port Set up
+const PORT = process.env.PORT || 3000;
+Sentry.setupExpressErrorHandler(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections and exceptions
